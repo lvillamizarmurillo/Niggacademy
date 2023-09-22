888125fae8d78752259aacad8f8a052e8ad0b7b2
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-09-22 05:24:38 -0500

    feat: :sparkles: Se hace el rate limit, se termina con la configuracion en el app.js y se automatizan las versiones con sus routers

--- api/app.js
+++ api/app.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import { loadEnv } from 'vite';
 import cors from 'cors';
+import rateLimit from './src/config/rateLimit.js';
+import routerDinamico from './src/routes/index.js';
 
-const env = loadEnv('development', process.cwd(), "VITE");
-const app = express();
+const env = loadEnv('development', process.cwd(), 'VITE');
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app
+    .use(rateLimit)
 
-const config = {
-    port: env.VITE_PORT_BACKEND,
-    hostname: env.VITE_HOSTNAME
-}
+    .use(cors())
 
-app.listen(config,()=>{
-    console.log(`http://${config.hostname}:${config.port}`);
-})
+    .use(express.json())
+
+    .use(async(req,res,next)=>{
+        try {
+            app.use('/niggacademy', await routerDinamico(req.headers('Accept-version')))
+        } catch (error) {
+            res.status(400).send({status: 400,message: 'Ingrese en los headers la version a utilizar para el api'})
+        }
+        next()
+    })
+
+    .listen(config, ()=>{
+        console.log(`http://${env.VITE_HOSTNAME}:${env.VITE_PORT_BACKEND}`);
+    })
