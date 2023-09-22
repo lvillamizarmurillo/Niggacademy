fdbf73cb711c019a2254a95bf4af1f61a19ce6e0
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-09-22 17:11:01 -0500

    feat: :sparkles: Se hace el jwt para que no solo encripte sino que verifique permisos a tablas y versiones

---
+++ api/src/middlewares/jwt.js
@@ -0,0 +1,43 @@
+import { loadEnv } from 'vite';
+import { jwtVerify, SignJWT } from 'jose';
+import { ObjectId } from 'mongodb';
+import db from '../config/connectMongo.js';
+
+const env = loadEnv('development', process.cwd(), 'JWT');
+const usuario = db.getInstancia().elegirColeccion('usuarios').conectar();
+const crearToken = async(req,res,next)=>{
+    if(Object.keys(req.body) === 0) return res.status(401).send('Datos no enviados')
+    const encoder = TextEncoder();
+    const result = await usuario.findOne({correo: req.body.email, password: req.body.password})
+    if(!result) return res.status(401).send({status:401,message:'Usuario no encontrado'})
+    const id = result._id.toString();
+    const jwtEncriptado = await new SignJWT({id: id})
+        .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+        .setIssuedAt()
+        .setExpirationTime('3h')
+        .sign(encoder.encode(env.JWT));
+    req.data = {status:200,message:jwtEncriptado};
+    next()
+}
+const verificarToken = async(req,token)=>{
+    const encoder = TextEncoder();
+    const jwtData = new jwtVerify(
+        token,
+        encoder.encode(env.JWT)
+    )
+    const baseQuitada = req.baseUrl.slice(12)
+    try {
+        const result = usuario.fidOne({
+            _id: new ObjectId(jwtData.payload.id),
+            [`permisos.${baseQuitada}`]: `${req.headers['accept-version']}`
+        })
+        return result
+    } catch (error) {
+        return false;
+    }
+}
+
+export {
+    crearToken,
+    verificarToken
+}
