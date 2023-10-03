b7401e26edec4a443548f643e770c849fb70ee45
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-10-03 16:24:21 -0500

    feat: :sparkles: Funcionalidad del apartado de secciones
    Se hace el front end y la funcionalidad de este apartado

--- src/components/views/Secciones.jsx
+++ src/components/views/Secciones.jsx
@@ -3,10 +3,41 @@ import Navegadora from '../global';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Secciones() {
     const navigate = useNavigate();
+    const [data, setData] = useState(null);
+    const [dataComment, setDataComment] = useState(null);
     const nombreCurso = useParams();
+    const fetchDataFromApi = async () => {
+        try {
+          const response = await(await fetch(`http://127.16.15.14:5072/niggacademy/contenido/${nombreCurso['*']}`, {
+              method: "GET",
+              headers: {
+                  'Accept-version': '1.0.0',
+                  'Authorization': `Bearer ${localStorage.token}`
+              }
+          })).json();
+          setData(response);
+        } catch (error) {
+          console.error(error);
+        }
+    };
+    const fetchDataFromApiComment = async () => {
+        try {
+          const response1 = await(await fetch(`http://127.16.15.14:5072/niggacademy/contenido/${nombreCurso['*']}`, {
+              method: "GET",
+              headers: {
+                  'Accept-version': '1.0.1',
+                  'Authorization': `Bearer ${localStorage.token}`
+              }
+          })).json();
+          setDataComment(response1);
+        } catch (error) {
+          console.error(error);
+        }
+    };
     const verifyToken = () => {
         if(!localStorage.token){
             navigate('/login');
@@ -15,63 +46,64 @@ function Secciones() {
       };
       useEffect(() => {
         verifyToken();
+        fetchDataFromApi();
+        fetchDataFromApiComment();
       }, []);
-    console.log(nombreCurso['*']);
     return (
         <>
             <Navegadora />
             <div className="contenedor-cursos">
-                <div className="caja-curso">
-                    <div className="nombre-curso">
-                        <h2><a href="">Nombre</a></h2>
-                    </div>
-                    <div className="descripcion-curso">
-                        <p>descripcion</p>
-                    </div>
-                </div>
+                    {data !== null ? (
+                        data.map((item,index)=>(
+                            <div className="caja-curso" key={index}>
+                                <div className="nombre-curso-seccion">
+                                    <h2><Link to={`/`}>{item.nombre}</Link></h2>
+                                </div>
+                                <div className="descripcion-curso">
+                                    <p>{item.descripcion}</p>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="wrapper">
+                            <div className="circle"></div>
+                            <div className="circle"></div>
+                            <div className="circle"></div>
+                            <div className="shadow"></div>
+                            <div className="shadow"></div>
+                            <div className="shadow"></div>
+                        </div>
+                    )}
             </div>
             <div className="comentarios">
                 <div className="titulo"><h1>Comentarios</h1></div>
                 <div className="comment">
                     <table>
-                        <tr>
-                            <td><h3>emailComment</h3></td>
-                        </tr>
-                        <tr>
-                            <td><h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta iure magnam reiciendis voluptate ducimus eum deleniti, ex, dolorum molestias cumque minus nostrum tempora. Error possimus harum aperiam, labore minus consequatur.</h4></td>
-                        </tr>
-                    </table>
-                    <table>
-                        <tr>
-                            <td><h3>emailComment</h3></td>
-                        </tr>
-                        <tr>
-                            <td><h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta iure magnam reiciendis voluptate ducimus eum deleniti, ex, dolorum molestias cumque minus nostrum tempora. Error possimus harum aperiam, labore minus consequatur.</h4></td>
-                        </tr>
-                    </table>
-                    <table>
-                        <tr>
-                            <td><h3>emailComment</h3></td>
-                        </tr>
-                        <tr>
-                            <td><h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta iure magnam reiciendis voluptate ducimus eum deleniti, ex, dolorum molestias cumque minus nostrum tempora. Error possimus harum aperiam, labore minus consequatur.</h4></td>
-                        </tr>
-                    </table>
-                    <table>
-                        <tr>
-                            <td><h3>emailComment</h3></td>
-                        </tr>
-                        <tr>
-                            <td><h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta iure magnam reiciendis voluptate ducimus eum deleniti, ex, dolorum molestias cumque minus nostrum tempora. Error possimus harum aperiam, labore minus consequatur.</h4></td>
-                        </tr>
-                    </table>
-                    <table>
-                        <tr>
-                            <td><h3>emailComment</h3></td>
-                        </tr>
-                        <tr>
-                            <td><h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta iure magnam reiciendis voluptate ducimus eum deleniti, ex, dolorum molestias cumque minus nostrum tempora. Error possimus harum aperiam, labore minus consequatur.</h4></td>
-                        </tr>
+                        <tbody>
+                            {dataComment !== null ? (
+                            dataComment.map((item, index) => (
+                                <React.Fragment key={index}>
+                                <tr>
+                                    <td><h3>{item.correo}</h3></td>
+                                </tr>
+                                <tr>
+                                    <td><h4>{item.descripcion}</h4></td>
+                                </tr>
+                                </React.Fragment>
+                            ))
+                            ) : (
+                            <tr>
+                                <td colSpan="2" className="wrapper">
+                                <div className="circle"></div>
+                                <div className="circle"></div>
+                                <div className="circle"></div>
+                                <div className="shadow"></div>
+                                <div className="shadow"></div>
+                                <div className="shadow"></div>
+                                </td>
+                            </tr>
+                            )}
+                        </tbody>
                     </table>
                 </div>
             </div>
